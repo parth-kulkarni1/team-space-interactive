b8abf543ae9ea660536d155cd96b8b75ee0bfceb
parth-kulkarni1 <parth.kulkarni2705@gmail.com>
2022-11-12 12:12:57 +1100

    Created node.js back-end with express, created react-front end and have implemented TypeORM within express..
    TO DO:
    
     - Start developing the landing page for website
     - Install Bootstrap

---
+++ express/src/app.ts
@@ -0,0 +1,52 @@
+import * as express from "express"
+import { Request, Response } from "express"
+import { User } from "./entity/user"
+import { myDataSource } from "./app-data-source"
+
+// establish database connection
+myDataSource
+    .initialize()
+    .then(() => {
+        console.log("Data Source has been initialized!x")
+    })
+    .catch((err) => {
+        console.error("Error during Data Source initialization:", err)
+    })
+
+// create and setup express app
+const app = express()
+app.use(express.json())
+
+// register routes
+app.get("/users", async function (req: Request, res: Response) {
+    const users = await myDataSource.getRepository(User).find()
+    res.json(users)
+})
+
+app.get("/users/:id", async function (req: Request, res: Response) {
+    const results = await myDataSource.getRepository(User).findOneBy({
+    })
+    return res.send(results)
+})
+
+app.post("/users", async function (req: Request, res: Response) {
+    const user = await myDataSource.getRepository(User).create(req.body)
+    const results = await myDataSource.getRepository(User).save(user)
+    return res.send(results)
+})
+
+app.put("/users/:id", async function (req: Request, res: Response) {
+    const user = await myDataSource.getRepository(User).findOneBy({
+    })
+    myDataSource.getRepository(User).merge(user, req.body)
+    const results = await myDataSource.getRepository(User).save(user)
+    return res.send(results)
+})
+
+app.delete("/users/:id", async function (req: Request, res: Response) {
+    const results = await myDataSource.getRepository(User).delete(req.params.id)
+    return res.send(results)
+})
+
+// start express server
+app.listen(4000)
